72a6ebc28b55e786064ca804ae22d3cf685fbc6c
Ryskeldi23 <ahmatbekovyryskeldigh@gmail.com>
2025-01-12 11:48:37 +0600

    first pycharm commit

---
+++ main.py
@@ -0,0 +1,13 @@
+name = input()
+price = float(input())
+weight = float(input())
+money = int(input())
+
+ret = price * weight
+result = money - ret
+
+print("Чек")
+print(f"{name} - {int(weight)}кг - {int(price)}руб/кг")
+print(f"Итого: {int(ret)}руб")
+print(f"Внесено: {int(money)}руб")
+print(f"Сдача: {int(result)}руб")
